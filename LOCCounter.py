1b306c596202578154bcfd5b0d03adaa51af6ae5
Bryce Vonilten <ki6bpd@gmail.com>
2019-11-07 12:41:02 -0800

    added docstrings and improved inline documentation

--- LOCCounter.py
+++ LOCCounter.py
@@ -1,8 +1,18 @@
 import string
 import re
 
-# Define a class
 class LOCCounter:
+	"""
+	Given a filename, this class will be able to get LOC metrics by calling getLOC()
+	
+	Args:
+        file (str): File path to .py file that will be analyzed
+
+	Methods:
+		getLOC(): Calculates and returns LOC
+		choseRegexGroups(match): helper function to filter regex matches
+	"""
+	
 	def __init__(self, file):
 		self.file = file
 
@@ -10,13 +20,30 @@ class LOCCounter:
 		#main()
 		
 	def getLOC():
-		source_loc = 0 #Source lines - includes end of source symbols like {}
-		single_comment_loc = 0 # Comments made using pound key
-		single_docstring_loc = 0 # ''' docstrings
-		double_docstring_loc = 0 # """ docstrings
-		total_comments_loc = 0 #Summation of other comment fields. Note: if single_docstring and double_docstring overlap it will count twice
-		blank_loc = 0 # Anything that consists of string.whitespace
-		total_line_count = 0 #No distinguishing - just the number of lines
+		""" Opens file, calculates LOC metrics
+		
+		Args:
+		
+		Returns:
+			dict:
+			{
+				source_loc, #Includes end of source symbols like {}
+				single_comment_loc, #Comments made using pound key
+				single_docstring_loc, #Docstring using single quotes 
+				double_docstring_loc, #Docstring using double quotes 
+				total_comments_loc, #Summation of other comment fields. Note: if single_docstring and double_docstring overlap it will count twice
+				blank_loc = 0, #Anything that consists of string.whitespace
+				total_line_count, #No distinguishing - just the number of lines
+			}
+		"""
+	
+		source_loc = 0
+		single_comment_loc = 0 
+		single_docstring_loc = 0 
+		double_docstring_loc = 0 
+		total_comments_loc = 0 
+		blank_loc = 0 
+		total_line_count = 0
 
 		with open(self.file, "r") as fp:
 			line = fp.readline()
@@ -64,15 +91,26 @@ class LOCCounter:
 		#print ("total_comments_loc: " + str(total_comments_loc))
 		#print ("blank_loc: " + str(blank_loc))
 		#print ("total_line_count: " + str(total_line_count))
+		
 		return {"source_loc": source_loc, "single_comments_loc": single_comments_loc,
 		"single_docstring_loc": single_docstring_loc, "double_docstring_loc": double_docstring_loc,
 		"total_comments_loc": total_comments_loc, "blank_loc": blank_loc,
 		"total_line_count": total_line_count}
 		
 	def choseRegexGroups(match):
+		""" Helper function used to filter docstrings when getLOC() removes quotes
+		
+		Args:
+			param1 (match): match object that needs to be filtered
+		Returns:
+			 string: If the match is not a docstring, return its substitute string. Else, return an empty string.
+		"""
+		
 		m = match.group()
 
 		if m is not None and (m=='"""' or m=="'''"):
 			return m 
 		else:
 			return ''
+			
+help(LOCCounter)
