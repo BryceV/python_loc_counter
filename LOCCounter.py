3699765ecb072ad412057111a11423cf5a85a35a
Bryce Vonilten <ki6bpd@gmail.com>
2019-11-07 12:44:54 -0800

    Removed help() call that as left in and fixed refs to self. Moved metrics to private scope

--- LOCCounter.py
+++ LOCCounter.py
@@ -19,7 +19,15 @@ class LOCCounter:
 	#if __name__ == '__main__':
 		#main()
 		
-	def getLOC():
+	self.__source_loc = 0
+	self.__single_comment_loc = 0 
+	self.__single_docstring_loc = 0 
+	self.__double_docstring_loc = 0 
+	self.__total_comments_loc = 0 
+	self.__blank_loc = 0 
+	self.__total_line_count = 0
+		
+	def getLOC(self):
 		""" Opens file, calculates LOC metrics
 		
 		Args:
@@ -36,14 +44,6 @@ class LOCCounter:
 				total_line_count, #No distinguishing - just the number of lines
 			}
 		"""
-	
-		source_loc = 0
-		single_comment_loc = 0 
-		single_docstring_loc = 0 
-		double_docstring_loc = 0 
-		total_comments_loc = 0 
-		blank_loc = 0 
-		total_line_count = 0
 
 		with open(self.file, "r") as fp:
 			line = fp.readline()
@@ -51,11 +51,11 @@ class LOCCounter:
 			inSingleDoc = False
 
 			while line:		
-				total_line_count +=1
+				self.__total_line_count +=1
 				
 				#Blank lines in docstrings only count as blank lines
 				if line.strip(string.whitespace)=='':
-					blank_loc += 1
+					self.__blank_loc += 1
 					line = fp.readline()
 					continue
 				
@@ -67,37 +67,37 @@ class LOCCounter:
 				inSingleDoc = inSingleDoc if (l.count("'''")%2 == 0) else not inSingleDoc
 
 				if "#" in l:
-					single_comment_loc += 1
+					self.__single_comment_loc += 1
 					
 				if ('"""' in l) or inDoubleDoc:
-					double_docstring_loc += 1
+					self.__double_docstring_loc += 1
 					
 				if ("'''" in l) or inSingleDoc:
-					single_docstring_loc += 1
+					self.__single_docstring_loc += 1
 					
 				#This regex will not work without removing comments first
 				sourceTest = re.sub(r"((\"\"\")|(''')|(#)).*\1?", "", l).strip()
 				if ((sourceTest!='') and (not inDoubleDoc) and (not inSingleDoc)):
-					source_loc += 1
+					self.__source_loc += 1
 
 				line = fp.readline()
 				
-		total_comments_loc = single_docstring_loc + double_docstring_loc + single_comment_loc
+		self.__total_comments_loc = self.__single_docstring_loc + self.__double_docstring_loc + self.__single_comment_loc
 		
-		#print ("source_loc: " + str(source_loc))
-		#print ("single_comments_loc: " + str(single_comment_loc))
-		#print ("single_docstring_loc: " + str(single_docstring_loc))
-		#print ("double_docstring_loc: " + str(double_docstring_loc))
-		#print ("total_comments_loc: " + str(total_comments_loc))
-		#print ("blank_loc: " + str(blank_loc))
-		#print ("total_line_count: " + str(total_line_count))
+		#print ("source_loc: " + str(self.__source_loc))
+		#print ("single_comments_loc: " + str(self.__single_comment_loc))
+		#print ("single_docstring_loc: " + str(self.__single_docstring_loc))
+		#print ("double_docstring_loc: " + str(self.__double_docstring_loc))
+		#print ("total_comments_loc: " + str(self.__total_comments_loc))
+		#print ("blank_loc: " + str(self.__blank_loc))
+		#print ("total_line_count: " + str(self.__total_line_count))
 		
-		return {"source_loc": source_loc, "single_comments_loc": single_comments_loc,
-		"single_docstring_loc": single_docstring_loc, "double_docstring_loc": double_docstring_loc,
-		"total_comments_loc": total_comments_loc, "blank_loc": blank_loc,
-		"total_line_count": total_line_count}
+		return {"source_loc": self.__source_loc, "single_comments_loc": self.__single_comments_loc,
+		"single_docstring_loc": single_docstring_loc, "double_docstring_loc": self.__double_docstring_loc,
+		"total_comments_loc": self.__total_comments_loc, "blank_loc": self.__blank_loc,
+		"total_line_count": self.__total_line_count}
 		
-	def choseRegexGroups(match):
+	def choseRegexGroups(self, match):
 		""" Helper function used to filter docstrings when getLOC() removes quotes
 		
 		Args:
@@ -112,5 +112,3 @@ class LOCCounter:
 			return m 
 		else:
 			return ''
-			
-help(LOCCounter)
